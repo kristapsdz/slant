6003585e8e6eb92699693b06bee635f992860745
kristaps <>
2018-06-05 15:22:07 +0000

    Use configured variables.

--- slant-upgrade.in.sh
+++ slant-upgrade.in.sh
@@ -1,24 +1,31 @@
 #! /bin/sh
 
-if [ ! -f "/var/www/data/slant.db" ]
+if [ -n "`pgrep slant-collectd`" ]
 then
-	echo "/var/www/data/slant.db: installing new"
-	kwebapp-sql "@SHAREDIR@/slant/slant.kwbp" | sqlite3 "/var/www/data/slant.db"
-	install -m 0444  "@SHAREDIR@/slant/slant.kwbp" "/var/www/data/slant.kwbp"
-	exit 0
-fi
-
-echo "/var/www/data/slant.db: patching existing"
-
-kwebapp-sqldiff "/var/www/data/slant.kwbp"  "@SHAREDIR@/slant/slant.kwbp" > "/var/www/data/slant-upgrade.sql"
-
-if [ $? -ne 0 ]
-then
-	rm -f "/var/www/data/slant-upgrade.sql"
-	echo "/var/www/data/slant.db: patch aborted"
+	echo "slant-collectord running: stop it first" 1>&2
 	exit 1
 fi
 
-sqlite3 "/var/www/data/slant.db" < "/var/www/data/slant-upgrade.sql"
-install -m 0444  "@SHAREDIR@/slant/slant.kwbp" "/var/www/data/slant.kwbp"
-rm -f "/var/www/data/slant-upgrade.sql"
+if [ ! -f "@DATADIR@/slant.db" ]
+then
+	echo "@DATADIR@/slant.db: installing new"
+	kwebapp-sql "@SHAREDIR@/slant/slant.kwbp" | sqlite3 "@DATADIR@/slant.db"
+	install -m 0444  "@SHAREDIR@/slant/slant.kwbp" "@DATADIR@/slant.kwbp"
+	exit 0
+fi
+
+echo "@DATADIR@/slant.db: patching existing"
+
+kwebapp-sqldiff "@DATADIR@/slant.kwbp"  "@SHAREDIR@/slant/slant.kwbp" > "@DATADIR@/slant-upgrade.sql"
+
+if [ $? -ne 0 ]
+then
+	rm -f "@DATADIR@/slant-upgrade.sql"
+	echo "@DATADIR@/slant.db: patch aborted" 1>&2
+	exit 1
+fi
+
+sqlite3 "@DATADIR@/slant.db" < "@DATADIR@/slant-upgrade.sql"
+install -m 0444  "@SHAREDIR@/slant/slant.kwbp" "@DATADIR@/slant.kwbp"
+rm -f "@DATADIR@/slant-upgrade.sql"
+echo "@DATADIR@/slant.db: patch success"
