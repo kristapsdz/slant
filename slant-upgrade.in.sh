ad1ce20b1ad0f7c6f8c3e2d82c223f741e75d276
kristaps <>
2018-06-05 12:27:37 +0000

    Use SHAREDIR.

--- slant-upgrade.in.sh
+++ slant-upgrade.in.sh
@@ -3,14 +3,14 @@
 if [ ! -f "/var/www/data/slant.db" ]
 then
 	echo "/var/www/data/slant.db: installing new"
-	kwebapp-sql "/usr/local/share/slant/slant.kwbp" | sqlite3 "/var/www/data/slant.db"
-	install -m 0444  "/usr/local/share/slant/slant.kwbp" "/var/www/data/slant.kwbp"
+	kwebapp-sql "@SHAREDIR@/slant/slant.kwbp" | sqlite3 "/var/www/data/slant.db"
+	install -m 0444  "@SHAREDIR@/slant/slant.kwbp" "/var/www/data/slant.kwbp"
 	exit 0
 fi
 
 echo "/var/www/data/slant.db: patching existing"
 
-kwebapp-sqldiff "/var/www/data/slant.kwbp"  "/usr/local/share/slant/slant.kwbp" > "/var/www/data/slant-upgrade.sql"
+kwebapp-sqldiff "/var/www/data/slant.kwbp"  "@SHAREDIR@/slant/slant.kwbp" > "/var/www/data/slant-upgrade.sql"
 
 if [ $? -ne 0 ]
 then
@@ -20,5 +20,5 @@ then
 fi
 
 sqlite3 "/var/www/data/slant.db" < "/var/www/data/slant-upgrade.sql"
-install -m 0444  "/usr/local/share/slant/slant.kwbp" "/var/www/data/slant.kwbp"
+install -m 0444  "@SHAREDIR@/slant/slant.kwbp" "/var/www/data/slant.kwbp"
 rm -f "/var/www/data/slant-upgrade.sql"
