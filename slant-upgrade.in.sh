cffdbec1890f2634202065fb94841a1e5bce0e8e
kristaps <>
2019-05-13 13:23:40 +0000

    Link to ort, not kwebapp.  Allow -f to override installed location.

--- slant-upgrade.in.sh
+++ slant-upgrade.in.sh
@@ -17,6 +17,27 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
 
+KWBP="@SHAREDIR@/slant/slant.kwbp"
+
+args=`getopt f: $*`
+if [ $? -ne 0 ]
+then
+	echo 'Usage: ...'
+	exit 2
+fi
+set -- $args
+while [ $# -ne 0 ]
+do
+	case "$1" in
+	-f)
+		KWBP="$2"; shift; shift;;
+	--)
+		shift; break;;
+        esac
+done
+
+echo "$KWBP"
+
 if [ ! -f "@DATADIR@/slant.db" ]
 then
 	# If the database doesn't exist, obviously nothing's running.
@@ -24,10 +45,10 @@ then
 	set -e
 	mkdir -p "@DATADIR@"
 	echo "@DATADIR@/slant.db: installing new"
-	ort-sql "@SHAREDIR@/slant/slant.kwbp" | sqlite3 "@DATADIR@/slant.db"
+	ort-sql "$KWBP" | sqlite3 "@DATADIR@/slant.db"
 	chown www "@DATADIR@/slant.db"
 	chmod 600 "@DATADIR@/slant.db"
-	install -m 0444  "@SHAREDIR@/slant/slant.kwbp" "@DATADIR@/slant.kwbp"
+	install -m 0444 "$KWBP" "@DATADIR@/slant.kwbp"
 	exit 0
 fi
 
@@ -38,7 +59,7 @@ trap "rm -f $TMPFILE" ERR EXIT
 echo "@DATADIR@/slant.db: patching existing"
 
 ( echo "BEGIN EXCLUSIVE TRANSACTION;" ; \
-  ort-sqldiff "@DATADIR@/slant.kwbp"  "@SHAREDIR@/slant/slant.kwbp" ; \
+  ort-sqldiff "@DATADIR@/slant.kwbp"  "$KWBP" ; \
   echo "COMMIT TRANSACTION;" ; ) > $TMPFILE
 
 if [ $? -ne 0 ]
@@ -48,6 +69,6 @@ then
 fi
 
 sqlite3 "@DATADIR@/slant.db" < $TMPFILE
-install -m 0444  "@SHAREDIR@/slant/slant.kwbp" "@DATADIR@/slant.kwbp"
+install -m 0444 "$KWBP" "@DATADIR@/slant.kwbp"
 rm -f "@DATADIR@/slant-upgrade.sql"
 echo "@DATADIR@/slant.db: patch success"
