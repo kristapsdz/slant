85f363766a81cf50d68541e1cf89074cb66099c5
kristaps <>
2019-05-13 10:55:42 +0000

    Don't touch non-database files, don't restart daemon.  This suggested on the OpenBSD ports mailing list as obeying the principle of least surprise.

--- slant-upgrade.in.sh
+++ slant-upgrade.in.sh
@@ -24,56 +24,21 @@ then
 	set -e
 	mkdir -p "@DATADIR@"
 	echo "@DATADIR@/slant.db: installing new"
-	kwebapp-sql "@SHAREDIR@/slant/slant.kwbp" | sqlite3 "@DATADIR@/slant.db"
+	ort-sql "@SHAREDIR@/slant/slant.kwbp" | sqlite3 "@DATADIR@/slant.db"
 	chown www "@DATADIR@/slant.db"
 	chmod 600 "@DATADIR@/slant.db"
 	install -m 0444  "@SHAREDIR@/slant/slant.kwbp" "@DATADIR@/slant.kwbp"
-	chmod 555 "@CGIBIN@/slant-cgi"
 	exit 0
 fi
 
-# If the database exists, make sure we're not still writing to it.
-# First try to use rcctl; then make sure we're not running debug
-# instances.
-
-stopped=0
-rcctl check slant >/dev/null
-if [ $? -eq 0 ]
-then
-	echo "slant(rcctl) running: stopping it first" 1>&2
-	rcctl stop slant
-	if [ $? -ne 0 ]
-	then
-		echo "slant(rcctl) cannot be stopped" 1>&2
-		exit 1
-	fi
-	stopped=1
-else
-	pgrep slant-collectd >/dev/null
-	if [ $? -eq 0 ]
-	then
-		echo "slant-collectd running: stopping it first" 1>&2
-		stopped=2
-		pkill slant-collectd
-		sleep 2
-		pgrep slant-collectd >/dev/null
-		if [ $? -eq 0 ]
-		then
-			echo "slant-collectd cannot be stopped" 1>&2
-			exit 1
-		fi
-	fi
-fi
-
 set -e
-
 TMPFILE=`mktemp` || exit 1
 trap "rm -f $TMPFILE" ERR EXIT
 
 echo "@DATADIR@/slant.db: patching existing"
 
 ( echo "BEGIN EXCLUSIVE TRANSACTION;" ; \
-  kwebapp-sqldiff "@DATADIR@/slant.kwbp"  "@SHAREDIR@/slant/slant.kwbp" ; \
+  ort-sqldiff "@DATADIR@/slant.kwbp"  "@SHAREDIR@/slant/slant.kwbp" ; \
   echo "COMMIT TRANSACTION;" ; ) > $TMPFILE
 
 if [ $? -ne 0 ]
@@ -84,15 +49,5 @@ fi
 
 sqlite3 "@DATADIR@/slant.db" < $TMPFILE
 install -m 0444  "@SHAREDIR@/slant/slant.kwbp" "@DATADIR@/slant.kwbp"
-chmod 555 "@CGIBIN@/slant-cgi"
 rm -f "@DATADIR@/slant-upgrade.sql"
 echo "@DATADIR@/slant.db: patch success"
-
-if [ "$stopped" -eq 1 ]
-then
-	echo "slant(rcctl): restarting" 1>&2
-	rcctl start slant
-elif [ "$stopped" -eq 2 ]
-then
-	echo "slant-collectd must be manually restarted" 1>&2
-fi
